2600146e7d10179b5876c2a7e87abddcd0816440
TimWitzdam <tim2013w@gmail.com>
2025-02-24 20:58:01 +0100

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontSize: {
+      "4xl": [
+        "2.5rem",
+        {
+          lineHeight: "2.8rem",
+          letterSpacing: "-0.05rem",
+        },
+      ],
+      "3xl": ["2rem", { lineHeight: "2.4rem", letterSpacing: "-0.024rem" }],
+      "2xl": [
+        "1.5rem",
+        { lineHeight: "1.725rem", letterSpacing: "−0.0075rem" },
+      ],
+      base: ["1rem", { lineHeight: "1.3rem", letterSpacing: "0.012rem" }],
+    },
+    extend: {
+      colors: {
+        primary: "#FF634D",
+        red: "#FF453A",
+        yellow: "#FF9F0A",
+        green: "#30D158",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
